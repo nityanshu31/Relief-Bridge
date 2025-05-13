f5b49f502391bc293740736be22f555fb927d1cd
Nityanshu Ranawat <64055499+nityanshu31@users.noreply.github.com>
2025-05-13 13:19:52 +0530

    Update EditProfile.jsx

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -66,7 +66,7 @@ const EditProfile = () => {
     setMessage("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/edit-profile", {
+      const response = await fetch("https://backend-l7fu.onrender.com/api/auth/edit-profile", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
